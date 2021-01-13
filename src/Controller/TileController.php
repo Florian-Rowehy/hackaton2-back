79d56798f029d136fc64526461cb61cdbe5734dd
Florian-Rowehy <florian.rowehy@gmail.com>
2021-01-13 16:16:27 +0100

    created TileController, created index route

---
+++ src/Controller/TileController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\TileRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/api/tiles", name="tile_")
+ */
+class TileController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     */
+    public function index(TileRepository $tileRepository): Response
+    {
+        $tiles = $tileRepository->findAll();
+        return $this->json($tiles);
+    }
+}
