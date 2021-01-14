3ef07767791b76d172c386f638f541ff5cfb0b0e
Florian-Rowehy <florian.rowehy@gmail.com>
2021-01-14 12:12:19 +0100

    updated SecurityController

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -17,16 +18,17 @@ class SecurityController extends AbstractController
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
-
-        // get the login error if there is one
+        if ($this->getUser()) {
+            $user = $this->getUser();
+            return $this->json([
+                'username' => $user->getUsername(),
+                'roles' => $user->getRoles(),
+            ]);
+        }
         $error = $authenticationUtils->getLastAuthenticationError();
-        // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        //return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
         return $this->json(['last_username' => $lastUsername, 'error' => $error]);
     }
 
